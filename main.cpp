c6183790995faf451baf55912a94246fecaa740a
Aaditya Jha <45432533+Aadityaza@users.noreply.github.com>
2024-03-25 15:44:36 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
-#include<cmath> 
+#include <iostream>
+#include <cmath>
 #include <functional>
-#include<set>
+#include <set>
+#include <vector>
 
 using namespace std;
 
@@ -10,90 +11,76 @@ public:
     double data;
     double grad;
     set<Value*> prev;
-    function<void()> backward;
-    
-    
-    Value(): grad(0), prev() {}
-    
-    Value(double data, initializer_list<Value*> children = {}) 
-    : data(data), grad(0), prev(children) {}
+    function<void()> backwardFunction;
 
-    
-    double getGrad() {
-        return grad;
-    }
-    double getValue() {
-        return data;
-    }
+    // Constructors
+    Value() : data(0), grad(0), prev(), backwardFunction([](){ /* do nothing */ }) {}
+    Value(double data, const initializer_list<Value*>& children = {})
+        : data(data), grad(0), prev(children), backwardFunction([](){ /* do nothing */ }) {}
 
-    
+    // Getters
+    double getGrad() const { return grad; }
+    double getValue() const { return data; }
 
-    Value operator+( Value& other) {
+    // Operator overloadings
+    Value operator+(Value& other) {
         Value out;
         out.data = this->data + other.data;
-        out.backward =[this,&out,&other](){
-            this->grad += out.grad;
-            other.grad += out.grad;
-        };
-        cout<<"backward of +"<<endl;
+        cout << "assigning backward for +" << endl;
+        out.backwardFunction = bind(&Value::backwardAdd, this, ref(other), ref(out));
         return out;
     }
-    
-    Value operator-( Value& other) {
-        Value out;
-        out.data = this->data - other.data;
-        out.backward =[this,&out,&other](){
-            this->grad += out.grad;
-            other.grad -= out.grad;
-        };
-        return out;
+
+    void backwardAdd(Value& other, Value& out) {
+        cout << "running backward for +" << endl;
+        cout << "Incrementing gradient of this (a): " << this->grad << " by " << out.grad << endl;
+        this->grad += out.grad;
+        cout << "Resulting gradient of this (a): " << this->grad << endl;
+        cout << "Incrementing gradient of other (b): " << other.grad << " by " << out.grad << endl;
+        other.grad += out.grad;
+        cout << "Resulting gradient of other (b): " << other.grad << endl;
+        // No need to modify 'out.grad' here
     }
-    
-    Value operator*( Value& other) {
-        Value out;
-        out.data = this->data * other.data;
-        out.backward =[this,&out,&other](){
-            this->grad += other.data * out.grad;
-            other.grad += this->data * out.grad;
+
+    void backward() {
+        cout << "vos" << endl;
+        vector<Value*> topo;
+        set<Value*> visited;
+        const function<void(Value*)> buildTopo = [&](Value* v) {
+            if (visited.count(v) == 0) {
+                visited.insert(v);
+                for (const auto child : v->prev) {
+                    buildTopo(child);
+                }
+                topo.push_back(v);
+            }
         };
-        return out;
-    }
-    
-    Value operator^(const Value& other) {
-        Value out;
-        out.data = pow(this->data, other.data);
-        out.backward =[this,&out,&other](){
-            this->grad += other.data * pow(this->data, other.data - 1) * out.grad;
-        };
-        return out;
-    }
-    
-    Value relu(){
-        if(data < 0){
-            return Value(0);
+
+        buildTopo(this);
+
+        grad = 1;
+        for (auto it = topo.rbegin(); it != topo.rend(); ++it) {
+            cout << (*it)->data << endl;
+            (*it)->backwardFunction();
+
         }
-        else{
-            return *this;
-        }
-    }  
+    }
 };
 
 int main() {
-    // Creating object 'a' with value 4
     Value a(4);
-    Value b(9);
-    Value c; 
-
-    c = (a + b) *a;  
+    Value b(2);
+    Value c;
+    cout << "-----------------" << endl;
+    c = a + b + a;
+    cout << "-----------------" << endl;
     cout << "Value of c after calculation: " << c.getValue() << endl;
-
-    // Printing gradient of 'a'
     cout << "Gradient of a: " << a.getGrad() << endl;
-
-    // Performing backward propagation
-    c.backward();     
-    cout << "Gradient of a after backward propagation: " << a.getGrad() << endl;    
-
+    c.backward();
+    cout << "Gradient of a after backward propagation: " << a.getGrad() << endl;
+    cout << "c data = " << c.getValue() << endl;
+    cout << "c grad = " << c.getGrad() << endl;
+    cout << "a grad = " << a.getGrad() << endl;
+    cout << "b grad = " << b.getGrad() << endl;
     return 0;
 }
-
